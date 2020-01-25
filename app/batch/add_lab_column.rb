038c4c9b8fd5b756cfdc3d5222c4d21d54cdeb4b
shio-yaamaa <shio.yama520@gmail.com>
2020-01-25 23:02:40 +0900

    Delete add_lab_column.rb

--- app/batch/add_lab_column.rb
+++
@@ -1,9 +0,0 @@
-include Color
-
-Hattori.all.each do |hattori|
-  lab = rgb2lab([hattori.r, hattori.g, hattori.b])
-  hattori.l_star = lab[0]
-  hattori.a_star = lab[1]
-  hattori.b_star = lab[2]
-  hattori.save
-end
